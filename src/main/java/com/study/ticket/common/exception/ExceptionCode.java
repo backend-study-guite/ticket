319b1916a2ee1b8d3841090eb4efa04edfd87c81
장욱 <janguk95@naver.com>
2026-01-04 19:31:28 +0900

    feat: 레디스 캐싱 적용

--- src/main/java/com/study/ticket/common/exception/ExceptionCode.java
+++ src/main/java/com/study/ticket/common/exception/ExceptionCode.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ExceptionCode {
+
+    SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "처리 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요."),
+
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
 
     NOT_ENOUGH_POINTS(HttpStatus.BAD_REQUEST, "포인트가 부족합니다."),
